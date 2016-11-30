7d5c174ab69353a22bd4f236ecd85cac87516897
cschoi <neurochoi84@gmail.com>
2016-11-30 21:30:11 +0900

    20161129

---
+++ Part1/20161129/TF_모두의딥러닝/NN1_3_Deep.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import numpy as np
+
+xy = np.loadtxt('07train.txt', unpack=True)
+x_data = np.transpose(xy[0:-1])
+y_data = np.reshape(xy[-1], (4, 1))
+
+# Neural Network 1 For XOR Deep & Wide
+
+print x_data
+print y_data
+
+X = tf.placeholder(tf.float32, name='X-input')
+Y = tf.placeholder(tf.float32, name='Y-input')
+
+W1 = tf.Variable(tf.random_uniform([2, 5], -1.0, 1.0), name='Weight1')
+W2 = tf.Variable(tf.random_uniform([5, 4], -1.0, 1.0), name='Weight2')
+W3 = tf.Variable(tf.random_uniform([4, 1], -1.0, 1.0), name='Weight3')
+
+b1 = tf.Variable(tf.zeros([5]), name="Bias1")
+b2 = tf.Variable(tf.zeros([4]), name="Bias2")
+b3 = tf.Variable(tf.zeros([1]), name="Bias3")
+
+# Our hypothesis
+# h = tf.matmul(W, X)
+# hypothesis = tf.div(1., 1.+tf.exp(-h))
+L2 = tf.sigmoid(tf.matmul(X, W1) + b1)
+L3 = tf.sigmoid(tf.matmul(L2, W2) + b2)
+hypothesis = tf.sigmoid(tf.matmul(L3, W3) + b3)
+# cost function - cross entropy
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1-Y) * tf.log(1 - hypothesis))
+
+# Minimize
+a = tf.Variable(0.1) # Learning rate, alpha
+optimizer = tf.train.GradientDescentOptimizer(a)
+train = optimizer.minimize(cost)
+
+# Before starting, initialize the variables. We will 'run' this first.
+init = tf.initialize_all_variables()
+
+# Launch the graph.
+with tf.Session() as sess:
+    sess.run(init)
+
+	# Fit the line
+    for step in xrange(20000):
+        sess.run(train, feed_dict={X: x_data, Y: y_data})
+        if step % 200 == 0:
+            print step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W1), sess.run(W2)
+
+	# Test model
+    correct_prediction = tf.equal(tf.floor(hypothesis+0.5), Y)
+	# Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print sess.run([hypothesis, tf.floor(hypothesis+0.5), correct_prediction], feed_dict={X: x_data, Y: y_data})
+    print "Accuracy", accuracy.eval({X: x_data, Y: y_data})
